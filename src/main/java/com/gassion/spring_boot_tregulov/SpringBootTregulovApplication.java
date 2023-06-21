d1f23b8751b696bacc793f399de88709e03747f5
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-21 15:41:03 +0300

    init :

---
+++ src/main/java/com/gassion/spring_boot_tregulov/SpringBootTregulovApplication.java
@@ -0,0 +1,13 @@
+package com.gassion.spring_boot_tregulov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTregulovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTregulovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gassion/spring_boot_tregulov/SpringBootTregulovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gassion.spring_boot_tregulov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTregulovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
