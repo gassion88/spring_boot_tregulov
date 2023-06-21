3c8074c51e54beaf8f30a8ca58ebaaf49709660a
Soslan Gassiev <soslangassity04@gmail.com>
2023-06-21 18:52:46 +0300

    refactor : JPA

--- src/main/java/com/gassion/spring_boot_tregulov/dao/EmployeeDAOImpl.java
+++ src/main/java/com/gassion/spring_boot_tregulov/dao/EmployeeDAOImpl.java
@@ -2,7 +2,7 @@ package com.gassion.spring_boot_tregulov.dao;
 
 import com.gassion.spring_boot_tregulov.entity.Employee;
 import jakarta.persistence.EntityManager;
-import org.hibernate.Session;
+import jakarta.persistence.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -21,25 +21,34 @@ public class EmployeeDAOImpl implements EmployeeDAO{
 
     @Override
     public List<Employee> getAllEmployee() {
-        Session session = entityManager.unwrap(Session.class);
-        return session.createQuery("From Employee", Employee.class).getResultList();
+//        Session session = entityManager.unwrap(Session.class);
+//        return session.createQuery("From Employee", Employee.class).getResultList();
+
+        Query query = entityManager.createQuery("from Employee ");
+        return (List<Employee>) query.getResultList();
     }
 
     @Override
     public Employee getEmployeeByID(int employeeID) {
-        Session session = entityManager.unwrap(Session.class);
-        return session.get(Employee.class, employeeID);
+//        Session session = entityManager.unwrap(Session.class);
+//        return session.get(Employee.class, employeeID);
+
+        return entityManager.find(Employee.class, employeeID);
     }
 
     @Override
     public void saveOrUpdateEmployee(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
-        session.saveOrUpdate(employee);
+//        Session session = entityManager.unwrap(Session.class);
+//        session.saveOrUpdate(employee);
+
+        entityManager.merge(employee);
     }
 
     @Override
     public void deleteEmployeeID(int employeeID) {
-        Session session = entityManager.unwrap(Session.class);
-        session.createQuery("delete from Employee where id=:employeeID").setParameter("employeeID", employeeID).executeUpdate();
+//        Session session = entityManager.unwrap(Session.class);
+//        session.createQuery("delete from Employee where id=:employeeID").setParameter("employeeID", employeeID).executeUpdate();
+
+        entityManager.createQuery("delete from Employee where id=:employeeID").setParameter("employeeID", employeeID).executeUpdate();
     }
 }
